d77cdc73fa5f7b9be9a2039644ad5d6e7c63c123
sleep-lover <laleetsb2002@gmail.com>
2023-06-18 21:11:24 +0530

    organised the whole applicaion

--- app.js
+++ app.js
@@ -1,48 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose')
+require('./config/database').connect()
+require('dotenv').config()
+const port = process.env.PORT
+const express = require('express')
+const morgan = require('morgan')
+const blogRoutes = require('./routes/blogRoutes')
 
-const morgan= require('morgan');
-const { result } = require('lodash');
-const blogRoutes=require('./routes/blogRoutes')
-// express app
-const app = express();
-//connection to db
+const app = express()
 
-const dbURI = 'mongodb+srv://BloggIT:vyqnif-dyndoX-9gewzy@cluster0.limkl.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(dbURI)
-    .then((result) => {
-        console.log('connected to the db')
-    })
-    .catch((err) => {
-        console.log('not connected')
-    })
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
-// listen for requests
-//app.listen(3000);
-
-// register view engine
-app.set('view engine', 'ejs');
-app.set('views', 'viewsForApp')
-
-// middleware & static files
 app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
-app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
 app.use((req, res, next) => {
-  res.locals.path = req.path;
-  next();
-});
+  res.locals.path = req.path
+  next()
+})
 
 app.get('/', (req, res) => {
-   res.redirect('/blogs');
-});
+  res.redirect('/blogs')
+})
 
 app.get('/about', (req, res) => {
-    res.render('about', { title: 'About' });
-});
+  res.render('about', { title: 'About' })
+})
+
 app.use(blogRoutes)
- 
+
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404', { title: '404' });
-});
+  res.status(404).render('404', { title: '404' })
+})
+app.listen(8080, () => {
+  console.log(`listehing at ${port}`)
+})
--- models/bolg.js
+++ models/bolg.js
@@ -1,22 +1,23 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;//Schema is define the structure of the data model:is surround that
-const blogSchema = new Schema({
+const mongoose = require('mongoose')
+//Schema is define the structure of the data model:is surround that
+const blogSchema = mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     snippet: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     body: {
-        type: String,
-        required: true
-    }
-},
-    {
-        timestamps: true
-    }
-);
-const Blog =  mongoose.model('Blog',blogSchema)
-module.exports= Blog;
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+const Blog = mongoose.model('Blog', blogSchema)
+module.exports = Blog
